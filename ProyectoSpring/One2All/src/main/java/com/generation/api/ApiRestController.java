a9a66ba8045a3ac0778ecbdf9b087618bbef3c53
joao2730 <joao_1594@hotmail.com>
2022-07-05 22:11:15 -0400

    Creacion de api

---
+++ ProyectoSpring/One2All/src/main/java/com/generation/api/ApiRestController.java
@@ -0,0 +1,26 @@
+package com.generation.api;
+
+import com.generation.models.Marcador;
+import com.generation.services.MarcadorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ApiRestController {
+    //inyeccion de dependencia
+    @Autowired
+    MarcadorService marcadorService;
+
+    //localhost:9080/api/marcadores
+    @RequestMapping("/marcadores")
+    public List<Marcador> obtenerListaMarcadores() {
+        List<Marcador> listaMarcadores = marcadorService.findAll();//obtener lista de marcadores
+        return listaMarcadores;
+    }
+
+
+}
